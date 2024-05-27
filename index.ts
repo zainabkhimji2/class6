8509ee1381da1213c7efa8b258bf3406b10fdbc0
zainab khimji_zk <165508985+zainabkhimji2@users.noreply.github.com>
2024-05-27 12:52:30 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,170 @@
+// if (true){
+//     console.log("Hello");
+//     console.log("welcome");
+
+// }
+// *****************************************************************************************************
+
+// if (false){
+//     console.log("Hello");
+//     console.log("welcome");
+
+// }
+// *****************************************************************************************************
+
+// let isAssignmentComplted = true
+
+// if (isAssignmentComplted){
+//     console.log("welcome");
+
+// }
+
+// *****************************************************************************************************
+
+// let isAssignmentComplted = false;
+
+// if (isAssignmentComplted) {
+//   console.log("welcome");
+// }else{
+//     console.log("please complete your assignment");
+    
+// }
+
+// *****************************************************************************************************
+// let age = 18
+// // age > 18
+// if(age > 18){
+//     console.log("you are allowed");
+    
+// }else {
+//     console.log("you are not allowed");
+    
+// }
+// *****************************************************************************************************
+
+// let age = 23
+// // 23 > 18
+// if(age > 18){
+//     console.log("you are allowed");
+    
+// }else {
+//     console.log("you are not allowed");
+    
+// }
+// *****************************************************************************************************
+
+// let age = 18
+// // 18 >= 18
+// if(age >= 18){
+//     console.log("you are allowed");
+    
+// }else {
+//     console.log("you are not allowed");
+    
+// }
+
+// *****************************************************************************************************
+// let haveNiceJob = true;
+// let canCook = true;
+// if(haveNiceJob && canCook){
+// console.log("Rishta pakka !!!!");
+
+// }else{
+//     console.log("you are rejected");
+    
+// }
+// *****************************************************************************************************
+
+// let haveNiceJob = true;
+// let canCook = false;
+// if(haveNiceJob && canCook){
+// console.log("Rishta pakka !!!!");
+
+// }else{
+//     console.log("you are rejected");
+    
+// }
+// *****************************************************************************************************
+
+
+// let isHandsome = true;
+// let isRich = false;
+// if(isHandsome || isRich){
+// console.log("Rishta pakka !!!!");
+
+// }else{
+//     console.log("you are rejected");
+    
+    
+// }
+
+// *****************************************************************************************************
+
+// let isHandsome = false;
+// let isRich = false;
+// if(isHandsome || isRich){
+// console.log("Rishta pakka !!!!");
+
+// }else{
+//     console.log("you are rejected");
+    
+    
+// }
+
+// *****************************************************************************************************
+// let isHandsome = false;
+// let isRich = false;
+// let haveCivic = true;
+// let canCook = true;
+// if(isHandsome || isRich || haveCivic &&  canCook){
+// console.log("Rishta pakka !!!!");
+
+// }else{
+//     console.log("you are rejected");
+    
+    
+// }
+//*****************************************************************************************************
+// let  myPercentage = 101;
+// if(myPercentage >= 80 && myPercentage  <= 100){
+//     console.log("A_ONE GRADE");
+    
+// }else if(myPercentage >= 70 && myPercentage  <= 100){
+//     console.log("A GRADE");
+    
+// }else if(myPercentage >= 60 &&  myPercentage <= 100){
+//     console.log(" B GRADE");
+    
+// }else{
+//     console.log("you are fail");
+    
+// }
+// *****************************************************************************************************
+
+// let  myPercentage = 101;
+// if(myPercentage >= 80 && myPercentage  <= 100){
+//     console.log("A_ONE GRADE"); //false
+    
+// }else if(myPercentage >= 70 && myPercentage  <= 100){
+//     console.log("A GRADE");
+    
+// }else if(myPercentage >= 60 &&  myPercentage <= 100){
+//     console.log(" B GRADE");
+    
+// }else{
+//     console.log("you are fail");
+    
+// }
+// *****************************************************************************************************
+// *****************************************************************************************************
+// *****************************************************************************************************
+// *****************************************************************************************************
+
+// CONDITIONAL TERNARY OPERATOR
+
+
+
+//  *****************************************************************************************************
+// *****************************************************************************************************
+// *****************************************************************************************************
+// *****************************************************************************************************
